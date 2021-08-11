aeae3084f263909d217f8078f714e40a2a084647
genie <genie@library>
2021-08-11 19:03:28 +0300

    i am not using tools.build - we are not tools

--- src/genie/core.clj
+++ src/genie/core.clj
@@ -8,7 +8,7 @@
    [clojure.spec.alpha :as s]
    [clojure.java.io :as io]
 
-   [clojure.tools.build.api :as build.api]))
+   ))
 
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
@@ -18,20 +18,4 @@
                       :main-ns nil}
         process-dir (-> (io/file (System/getProperty "user.dir")) (.getCanonicalPath))
         {:keys [uberjar-name main-ns]} (merge default-opts opts)]
-    (let [class-dir "out/classes"
-          version "1"
-          basis (build.api/create-basis {:project "deps.edn"})]
-      (build.api/copy-dir
-       {:src-dirs ["src"]
-        :target-dir class-dir})
-      (build.api/compile-clj {:basis basis
-                              :src-dirs ["src"]
-                              :class-dir class-dir
-                              :compile-opts {:direct-linking true}})
-      (build.api/uber
-       {:class-dir class-dir
-        :uber-file uberjar-name
-        :basis basis
-        :main (-> main-ns (str) (clojure.string/replace #"-" "_") (symbol))
-        :manifest {"Manifest-Version" "1"
-                   "Created-By" "library"}}))))
+    ))
