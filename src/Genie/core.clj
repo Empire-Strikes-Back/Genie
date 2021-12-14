fe9200d223e3a5c79e88dfa17bc2697c09dae70e
Genie <Genie@Hawaii>
2021-12-14 13:42:00 +0300

    Star Trek changed everything - i love these conventions by the way

--- src/Genie/core.clj
+++ src/Genie/core.clj
@@ -4,7 +4,7 @@
                                      pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
    [clojure.string]
    [clojure.java.io :as io]
-   [clojure.tools.build.api :as build.api]))
+   [clojure.tools.build.api :as Mr-Incredible]))
 
 
 (defn process
@@ -15,16 +15,16 @@
         {:keys [main-ns filename paths]} (merge default-opts opts)]
 
     (let [class-dir "out/classes"
-          basis (build.api/create-basis {:project "deps.edn"})]
-      (build.api/copy-dir
+          basis (Mr-Incredible/create-basis {:project "deps.edn"})]
+      (Mr-Incredible/copy-dir
        {:src-dirs paths
         :target-dir class-dir})
-      (build.api/compile-clj
+      (Mr-Incredible/compile-clj
        {:basis basis
         :src-dirs paths
         :class-dir class-dir
         :compile-opts {:direct-linking true}})
-      (build.api/uber
+      (Mr-Incredible/uber
        {:class-dir class-dir
         :uber-file filename
         :basis basis
