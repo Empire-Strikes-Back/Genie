3ebf0c0f787f4ca5de6a1693ea59d86287e463d3
Genie <Genie@Hawaii>
2022-06-06 18:32:35 +0300

    i allow to pass options to clojure/tools.build :Jessie-Eisenberg Jay, he keeps hitting me in the face - is this how tv works?

--- src/Genie/core.clj
+++ src/Genie/core.clj
@@ -11,23 +11,38 @@
   [opts]
   (let [default-opts {:main-ns nil
                       :filename nil
-                      :paths nil}
-        {:keys [main-ns filename paths]} (merge default-opts opts)]
-
+                      :paths nil
+                      :create-basis-opts nil
+                      :copy-dir-opts nil
+                      :compile-clj-opts nil
+                      :uber-opts nil}
+        {:keys [main-ns
+                filename
+                paths
+                create-basis-opts
+                copy-dir-opts
+                compile-clj-opts
+                uber-opts]} (merge default-opts opts)]
     (let [class-dir "out/classes"
-          basis (Mr-Incredible/create-basis {:project "deps.edn"})]
+          basis (Mr-Incredible/create-basis (merge {:project "deps.edn"}
+                                                   create-basis-opts))]
       (Mr-Incredible/copy-dir
-       {:src-dirs paths
-        :target-dir class-dir})
+       (merge {:src-dirs paths
+               :target-dir class-dir}
+              copy-dir-opts))
       (Mr-Incredible/compile-clj
-       {:basis basis
-        :src-dirs paths
-        :class-dir class-dir
-        :compile-opts {:direct-linking true}})
+       (merge
+        {:basis basis
+         :src-dirs paths
+         :class-dir class-dir
+         :compile-opts {:direct-linking true}}
+        compile-clj-opts))
       (Mr-Incredible/uber
-       {:class-dir class-dir
-        :uber-file filename
-        :basis basis
-        :main (-> main-ns (str) (clojure.string/replace #"-" "_") (symbol))
-        :manifest {"Manifest-Version" "1"
-                   "Created-By" "library"}}))))
+       (merge
+        {:class-dir class-dir
+         :uber-file filename
+         :basis basis
+         :main (-> main-ns (str) (clojure.string/replace #"-" "_") (symbol))
+         :manifest {"Manifest-Version" "1"
+                    "Created-By" "library"}}
+        uber-opts)))))
